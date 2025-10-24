11b1e764f25fdba50adf9ab98a09242813783301
zubairkhawar565 <239949347+zubairkhawar565@users.noreply.github.com>
2025-10-24 18:01:40 +0500

    MK

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { persistor, store } from './store/Storeconfig.jsx'
-import { PersistGate } from 'redux-persist/integration/react'
+
 import './App.css'
 import './index.css'
 
